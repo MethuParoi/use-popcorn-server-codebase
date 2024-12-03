d967f48fade69eb12d7ba27e966c47466082cc69
MethuParoi <paroimethu@gmail.com>
2024-12-03 21:23:42 +0600

    connection established with mongodb

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
@@ -6,8 +8,39 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const rootRouter = require("./routes/index");
-app.use("/api/v1", rootRouter);
+//mongoDB connection
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@use-popcorn-cluster.fhpfd.mongodb.net/?retryWrites=true&w=majority&appName=use-popcorn-cluster`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+//-------------------------------------
+
+// const rootRouter = require("./routes/index");
+// app.use("/api/v1", rootRouter);
 
 app.get("/", (req, res) => {
   res.send("UsePopCorn server running");
@@ -18,5 +51,5 @@ app.listen(port, (err) => {
   if (err) {
     console.log(err);
   }
-  console.log(`listening on port: ${PORT}`);
+  console.log(`listening on port: ${port}`);
 });
